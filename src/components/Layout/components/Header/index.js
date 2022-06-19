675e8fd71bd9fd42487b80390844d967e5c600fe
Nguyen Quy <huyquy2003@gmail.com>
2022-06-19 09:58:24 +0700

    19/6/22__bai2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,72 @@
 
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
 
+import { ImSpinner2 } from 'react-icons/im';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { BsSearch } from 'react-icons/bs';
+
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss'
+import images from '~/assets/images';
+import AccountItem from '~/components/AccountItem';
 
 const cx = classNames.bind(styles)
 
 function Header() {
+
+    const [searchResult, setSearchResult] = useState([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([9, 2, 4])
+        }, 1000)
+    }, [])
+
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
-        </header>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+
+                <div>
+                    <Tippy
+                        interactive
+                        visible={searchResult.length > 0}
+                        placement="bottom-end"
+                        render={(attrs) => (
+                            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                                <PopperWrapper>
+                                    <h4 className={cx('search-title')}>Accounts</h4>
+                                    <AccountItem />
+                                    <AccountItem />
+                                    <AccountItem />
+                                    <AccountItem />
+                                </PopperWrapper>
+                            </div>
+                        )}
+                    >
+                        <div className={cx('search')}>
+                            <input placeholder="Search accounts and videos" spellCheck={false} />
+                            <button className={cx('clear')}>
+                                <AiFillCloseCircle />
+                            </button>
+
+                            {/* <button className={cx('loading')}>
+                                <ImSpinner2 />
+                            </button> */}
+
+                            <button className={cx('search-btn')}>
+                                <BsSearch />
+                            </button>
+                        </div>
+                    </Tippy>
+                </div>
+
+                <div className={cx('actions')}></div>
+            </div>
+        </header >
     );
 }
 
